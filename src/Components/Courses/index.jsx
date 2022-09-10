cb53ecb71b358f849cad9c82ec6fdb9dc6de596e
Boris <watasinonamaewa@gmail.com>
2022-09-10 15:44:11 +0400

    add Courses component

--- src/Components/Courses/index.jsx
+++ src/Components/Courses/index.jsx
@@ -2,11 +2,26 @@ import './Courses.css';
 import { Chips } from '../Chips';
 
 export function Courses() {
+  const chipsTexts = ['Графiчны дызайн', 'Motion дызайн', 'UX/UI дызайн', 'Фатаграфiя', 'I яшчэ'];
+  const courses = ['Школа “Калякi-Малякi”', 'Курс дызайну ад Акадэмii Мастацтвау', 'Школа “Такiя Сякiя”', 'Design Art School', 'I яшчэ'];
   return (
     <>
-      <Chips>
-        asfdge
-      </Chips>
+      <h2 className='courses-header'>Курсы</h2>
+      <div className='courses-chips'>
+        {chipsTexts.map((text, index) => (
+          <Chips key={index}>
+            {text}
+          </Chips>
+        ))}
+      </div>
+      <ul className='courses-list'>
+          {courses.map((text, index) => (
+            <li className='courses-list-item'>
+              <h3>{text}</h3>
+              <img src='./img/plus.png' alt='open'/>
+            </li>
+          ))}
+      </ul>
     </>
   )
 }
