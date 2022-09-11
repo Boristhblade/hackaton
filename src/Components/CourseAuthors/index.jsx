51f66b2d749238aa582d59f248a1af817da355ec
Boris <watasinonamaewa@gmail.com>
2022-09-11 17:59:04 +0400

    making course page

--- src/Components/CourseAuthors/index.jsx
+++ src/Components/CourseAuthors/index.jsx
@@ -2,9 +2,27 @@ import './CourseAuthors.css';
 import React from 'react';
 
 export function CourseAuthors() {
+  const data = [
+    ['Святлана Карповiч', 'аўтар і выкладчык курса'],
+    ['Кацярына Моцная', 'куратар праэкта'],
+    ['Вольга Кавальчук', 'дызайнер'],
+    ['Святлана Карповiч', 'аўтар і выкладчык курса'],
+    ['Святлана Карповiч', 'аўтар і выкладчык курса'],
+  ]
   return (
-    <div>
-      
+    <div className='courseauthors'>
+      <h3 className='courseauthors-header'>Каманда курса</h3>
+      <div className='courseauthors-container'>
+        {data.map(([name, role]) => {
+          return (
+            <div>
+              <img className='courseauthors-photo' alt='photo'/>
+              <h4>{name}</h4>
+              <p>{role}</p>
+            </div>
+          )
+        })}
+      </div>
     </div>
   )
 }
