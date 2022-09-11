53053896045bde499bac230e369e8e0a5aae06ed
Boris <watasinonamaewa@gmail.com>
2022-09-11 18:28:14 +0400

    fixes

--- src/Pages/Course.jsx
+++ src/Pages/Course.jsx
@@ -7,8 +7,15 @@ import { CourseDesc } from "../Components/CourseDesc";
 import { CourseBenefits } from "../Components/CourseBenefits";
 import { CourseSchedule } from "../Components/CourseSchedule";
 import { CourseAuthors } from "../Components/CourseAuthors";
+import axios from "axios";
 
 export function Course() {
+  const getData = async () => {
+    const { data } = await axios.get('https://hack.webink.site//wp-json/mr/v1/get/cources');
+    return data;
+  }
+  const data = getData();
+  console.log(data)
   return (
     <>
       <Header />
